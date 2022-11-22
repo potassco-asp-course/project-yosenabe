5dd2abe750ae77c42c81dbd8b8a1140534a3fc94
François Laferrière <45877269+FrancoisLaferriere@users.noreply.github.com>
2022-11-22 12:07:22 +0100

    Update test.py
    compatibility with clingo 5.6

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ SOLUTIONS = "solutions/"
 
 
 def call_clingo(input_names, timeout):
-    cmd = [CLINGO, "--warn=no-atom-undefined", "--warn=no-file-included", "--warn=no-operation-undefined", "--warn=no-variable-unbounded", "--warn=no-global-variable", "--outf=2"] + input_names
+    cmd = [CLINGO, "--warn=no-atom-undefined", "--warn=no-file-included", "--warn=no-operation-undefined", "--warn=no-global-variable", "--outf=2"] + input_names
     start = time.time()
     output = run(cmd, stdout=PIPE, stderr=PIPE, timeout=timeout)
     end = time.time()
